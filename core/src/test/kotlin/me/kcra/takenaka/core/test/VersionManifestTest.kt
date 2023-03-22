93bb25eb5a179af617215936bab601f5bb7aa2c8
Matous Kucera <mk@kcra.me>
2023-03-22 18:14:24 +0100

    test(core): workspace, utils, version manifest

--- core/src/test/kotlin/me/kcra/takenaka/core/test/VersionManifestTest.kt
+++ core/src/test/kotlin/me/kcra/takenaka/core/test/VersionManifestTest.kt
@@ -18,33 +18,65 @@
 package me.kcra.takenaka.core.test
 
 import com.fasterxml.jackson.module.kotlin.readValue
-import me.kcra.takenaka.core.SpigotVersionManifest
+import me.kcra.takenaka.core.VersionAttributes
 import me.kcra.takenaka.core.util.objectMapper
 import me.kcra.takenaka.core.versionManifest
 import java.net.URL
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
+import kotlin.test.*
 
 class VersionManifestTest {
     private val objectMapper = objectMapper()
 
     @Test
-    fun `fetch and deserialize the version manifest`() {
-        val manifest = objectMapper.versionManifest()
-
-        println(manifest)
-
-        assertNotNull(manifest["1.12.2"], message = "did not find 1.12.2 in the manifest")
-        assertNotNull(manifest["1.19"], message = "did not find 1.19 in the manifest")
+    fun `version manifest can be fetched and deserialized`() {
+        objectMapper.versionManifest()
     }
 
     @Test
-    fun `fetch and deserialize the spigot version manifest`() {
-        val spigotManifest = objectMapper.readValue<SpigotVersionManifest>(URL("https://hub.spigotmc.org/versions/1.19.2.json"))
+    fun `version manifest can be fetched and deserialized, version list is not empty`() {
+        val versionManifest = objectMapper.versionManifest()
+        assertTrue(versionManifest.versions.isNotEmpty())
+    }
 
-        println(spigotManifest)
+    @Test
+    fun `version can be found by id`() {
+        val versionManifest = objectMapper.versionManifest()
+        val version = versionManifest["1.14.4"]
+        assertNotNull(version)
+        assertEquals("1.14.4", version.id)
+    }
 
-        assertEquals(spigotManifest.refs.keys, setOf("BuildData", "Bukkit", "CraftBukkit", "Spigot"), message = "1.19.2 refs mismatch")
+    @Test
+    fun `version list can be sorted by release time`() {
+        val versionManifest = objectMapper.versionManifest()
+        val sortedVersions = versionManifest.versions.sorted()
+        assertTrue(sortedVersions.isNotEmpty())
+        assertTrue(sortedVersions.first().releaseTime <= sortedVersions.last().releaseTime)
+    }
+
+    @Test
+    fun `version attributes can be deserialized`() {
+        val versionManifest = objectMapper.versionManifest()
+        val version = versionManifest["1.14.4"]
+        assertNotNull(version)
+        objectMapper.readValue<VersionAttributes>(URL(version.url))
+    }
+
+    @Test
+    fun `server mappings are available for 1_14_4`() {
+        val versionManifest = objectMapper.versionManifest()
+        val version = versionManifest["1.14.4"]
+        assertNotNull(version)
+        val versionAttributes = objectMapper.readValue<VersionAttributes>(URL(version.url))
+        assertNotNull(versionAttributes.downloads.serverMappings)
+    }
+
+    @Test
+    fun `server mappings are available for 1_12_2`() {
+        val versionManifest = objectMapper.versionManifest()
+        val version = versionManifest["1.12.2"]
+        assertNotNull(version)
+        val versionAttributes = objectMapper.readValue<VersionAttributes>(URL(version.url))
+        assertNull(versionAttributes.downloads.serverMappings)
     }
 }
