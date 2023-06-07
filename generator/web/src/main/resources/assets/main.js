fbb831dda9bf60851c3ab80aa05c82cef0ffbdc0
Matous Kucera <mk@kcra.me>
2023-06-07 16:30:05 +0200

    refactor(generator-web): improve search

--- generator/web/src/main/resources/assets/main.js
+++ generator/web/src/main/resources/assets/main.js
@@ -110,12 +110,10 @@ const search = (query) => {
     }
 
     const results = [];
+    const hasPackage = newQuery.includes("/");
 
     klassLoop:
     for (const klass of classIndex) {
-        // limit search to 50 results, should be plenty
-        if (results.length >= 50) break;
-
         for (const ns in klass) {
             const klassName = klass[ns];
 
@@ -123,35 +121,21 @@ const search = (query) => {
                 if (!klassName.toLowerCase().includes(newQuery)) continue;
                 if (!predicates.every((p) => p(klass, ns, klassName))) continue;
 
-                const resultElem = document.createElement("div");
-                resultElem.classList.add("search-result");
-                resultElem.addEventListener("click", () => {
-                    window.location.pathname = `${baseUrl}/${Object.values(klass).find((e) => e != null)}.html`;
-                });
-
                 const lastSlashIndex = klassName.lastIndexOf("/");
-                const klassPackage = lastSlashIndex !== -1 ? klassName.substring(0, lastSlashIndex).replaceAll("/", ".") : null;
-                const simpleKlassName = lastSlashIndex !== -1 ? klassName.substring(lastSlashIndex + 1) : klassName;
+                const simpleName = lastSlashIndex !== -1 ? klassName.substring(lastSlashIndex + 1) : klassName;
 
-                const title = document.createElement("p");
-                title.classList.add("search-result-title");
-                title.innerText = simpleKlassName;
-                resultElem.appendChild(title);
-
-                if (klassPackage) {
-                    const packageSubtitle = document.createElement("p");
-                    packageSubtitle.classList.add("search-result-subtitle");
-                    packageSubtitle.innerText = `package: ${klassPackage}`;
-                    resultElem.appendChild(packageSubtitle);
-                }
-
-                const nsSubtitle = document.createElement("p");
-                nsSubtitle.classList.add("search-result-subtitle");
-                nsSubtitle.innerHTML = `namespace: <span style="color:${colors[ns]}">${ns}</span>`;
-                resultElem.appendChild(nsSubtitle);
+                // if a package is not specified, match only against simple class names, not fully qualified ones
+                if (!hasPackage && !simpleName.toLowerCase().includes(newQuery)) continue;
 
                 // more similar = lower number
-                results.push({ similarity: klassName.length - newQuery.length, element: resultElem });
+                results.push({
+                    klass: klass,
+                    ns: ns,
+                    simpleName: simpleName,
+                    packageName: lastSlashIndex !== -1 ? klassName.substring(0, lastSlashIndex).replaceAll("/", ".") : null,
+                    similarity: (hasPackage ? klassName.length : simpleName.length) - newQuery.length
+                });
+
                 // only show a class once, skip the other namespaces
                 continue klassLoop;
             }
@@ -159,5 +143,33 @@ const search = (query) => {
     }
 
     results.sort((a, b) => a.similarity - b.similarity);
-    resultsBox.replaceChildren(...(results.map((r) => r.element)));
+    resultsBox.replaceChildren(...(
+        // limit results to 50, should be plenty
+        results.slice(0, Math.min(results.length, 50)).map((r) => {
+            const resultElem = document.createElement("div");
+            resultElem.classList.add("search-result");
+            resultElem.addEventListener("click", () => {
+                window.location.pathname = `${baseUrl}/${Object.values(r.klass).find((e) => e != null)}.html`;
+            });
+
+            const title = document.createElement("p");
+            title.classList.add("search-result-title");
+            title.innerText = r.simpleName;
+            resultElem.appendChild(title);
+
+            if (r.packageName) {
+                const packageSubtitle = document.createElement("p");
+                packageSubtitle.classList.add("search-result-subtitle");
+                packageSubtitle.innerText = `package: ${r.packageName}`;
+                resultElem.appendChild(packageSubtitle);
+            }
+
+            const nsSubtitle = document.createElement("p");
+            nsSubtitle.classList.add("search-result-subtitle");
+            nsSubtitle.innerHTML = `namespace: <span style="color:${colors[r.ns]}">${r.ns}</span>`;
+            resultElem.appendChild(nsSubtitle);
+
+            return resultElem;
+        })
+    ));
 };
