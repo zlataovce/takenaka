a7f472e6aadf894af330b54f08199bc06ae5bbf3
Matouš Kučera <mk@kcra.me>
2024-12-01 15:37:18 +0100

    fix: correct boolean attribute handling

--- generator/web/src/main/kotlin/me/kcra/takenaka/generator/web/util/html.kt
+++ generator/web/src/main/kotlin/me/kcra/takenaka/generator/web/util/html.kt
@@ -1,4 +1,50 @@
+/*
+ * This file is part of takenaka, licensed under the Apache License, Version 2.0 (the "License").
+ *
+ * Copyright (c) 2023-2024 Matous Kucera
+ *
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.kcra.takenaka.generator.web.util
+
+/**
+ * A `<` (less than) character encoded for HTML usage.
+ */
+const val HTML_LT = "&lt;"
+
+/**
+ * A `>` (greater than) character encoded for HTML usage.
+ */
+const val HTML_GT = "&gt;"
+
+/**
+ * Escapes `<` and `>` characters in a string for HTML usage.
+ *
+ * @return the escaped string
+ */
+fun String.escapeHtml(): String = replace("<", HTML_LT).replace(">", HTML_GT)
+
+/**
+ * A `"` (double quotation mark) character encoded for HTML usage.
+ */
+const val HTML_QUOT = "&quot;"
+
+/**
+ * Escapes the `"` character in a string for usage in an HTML tag attribute value.
+ *
+ * @return the escaped string
+ */
+fun String.escapeHtmlAttribute(): String = replace("\"", HTML_QUOT)
 
 /**
  * Void element tag names.
@@ -13,20 +59,23 @@ val VOID_ELEMENTS = setOf("area", "base", "br", "col", "embed", "hr", "img", "in
 typealias HTMLBuilder = StringBuilder
 
 /**
+ * An HTML builder action.
+ */
+typealias HTMLBuilderBlock = HTMLBuilder.() -> Unit
+
+/**
  * Builds an HTML document.
  *
  * @param docType whether a DOCTYPE should be prepended
  * @param block the builder action
  * @return the built document
  */
-inline fun buildHTML(docType: Boolean = true, block: HTMLBuilder.() -> Unit): String {
-    return buildString {
-        if (docType) {
-            append("<!DOCTYPE html>")
-        }
-
-        block(this)
+inline fun buildHTML(docType: Boolean = true, block: HTMLBuilderBlock): String = buildString {
+    if (docType) {
+        append("<!DOCTYPE html>")
     }
+
+    block(this)
 }
 
 /**
@@ -39,12 +88,15 @@ inline fun buildHTML(docType: Boolean = true, block: HTMLBuilder.() -> Unit): St
 inline fun HTMLBuilder.tag(
     name: String,
     attrs: Map<String, Any?> = emptyMap(),
-    block: HTMLBuilder.() -> Unit = {},
+    block: HTMLBuilderBlock = {},
 ) {
-    append("<${name}")
+    append("<$name")
     attrs.forEach { (key, value) ->
-        if (value != null) {
-            append(" ${key}=\"${value}\"")
+        if (value == null || value == false) return@forEach
+
+        append(" $key") // https://developer.mozilla.org/en-US/docs/Glossary/Boolean/HTML
+        if (value !is Boolean) {
+            append("=\"${value.toString().escapeHtmlAttribute()}\"")
         }
     }
 
@@ -54,7 +106,7 @@ inline fun HTMLBuilder.tag(
         append(">")
 
         block(this)
-        append("</${name}>")
+        append("</$name>")
     }
 }
 
@@ -64,7 +116,7 @@ inline fun HTMLBuilder.tag(
  * @param lang the `lang` attribute
  * @param block the builder action
  */
-inline fun HTMLBuilder.html(lang: String? = null, block: HTMLBuilder.() -> Unit = {}) =
+inline fun HTMLBuilder.html(lang: String? = null, block: HTMLBuilderBlock = {}) =
     tag("html", mapOf("lang" to lang), block)
 
 /**
@@ -72,49 +124,49 @@ inline fun HTMLBuilder.html(lang: String? = null, block: HTMLBuilder.() -> Unit 
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.head(block: HTMLBuilder.() -> Unit = {}) = tag("head", block = block)
+inline fun HTMLBuilder.head(block: HTMLBuilderBlock = {}) = tag("head", block = block)
 
 /**
  * Appends a `<body>` tag.
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.body(block: HTMLBuilder.() -> Unit = {}) = tag("body", block = block)
+inline fun HTMLBuilder.body(block: HTMLBuilderBlock = {}) = tag("body", block = block)
 
 /**
  * Appends a `<title>` tag.
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.title(block: HTMLBuilder.() -> Unit = {}) = tag("title", block = block)
+inline fun HTMLBuilder.title(block: HTMLBuilderBlock = {}) = tag("title", block = block)
 
 /**
  * Appends a `<main>` tag.
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.main(block: HTMLBuilder.() -> Unit = {}) = tag("main", block = block)
+inline fun HTMLBuilder.main(block: HTMLBuilderBlock = {}) = tag("main", block = block)
 
 /**
  * Appends a `<h1>` tag.
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.h1(block: HTMLBuilder.() -> Unit = {}) = tag("h1", block = block)
+inline fun HTMLBuilder.h1(block: HTMLBuilderBlock = {}) = tag("h1", block = block)
 
 /**
  * Appends a `<h3>` tag.
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.h3(block: HTMLBuilder.() -> Unit = {}) = tag("h3", block = block)
+inline fun HTMLBuilder.h3(block: HTMLBuilderBlock = {}) = tag("h3", block = block)
 
 /**
  * Appends a `<h4>` tag.
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.h4(block: HTMLBuilder.() -> Unit = {}) = tag("h4", block = block)
+inline fun HTMLBuilder.h4(block: HTMLBuilderBlock = {}) = tag("h4", block = block)
 
 /**
  * Appends a `<p>` tag.
@@ -124,7 +176,7 @@ inline fun HTMLBuilder.h4(block: HTMLBuilder.() -> Unit = {}) = tag("h4", block 
  * @param style the `style` attribute
  * @param block the builder action
  */
-inline fun HTMLBuilder.p(onClick: String? = null, classes: String? = null, style: String? = null, block: HTMLBuilder.() -> Unit = {}) =
+inline fun HTMLBuilder.p(onClick: String? = null, classes: String? = null, style: String? = null, block: HTMLBuilderBlock = {}) =
     tag("p", mapOf("onclick" to onClick, "class" to classes, "style" to style), block)
 
 /**
@@ -132,7 +184,7 @@ inline fun HTMLBuilder.p(onClick: String? = null, classes: String? = null, style
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.pre(block: HTMLBuilder.() -> Unit = {}) = tag("pre", block = block)
+inline fun HTMLBuilder.pre(block: HTMLBuilderBlock = {}) = tag("pre", block = block)
 
 /**
  * Appends a `<table>` tag.
@@ -140,7 +192,7 @@ inline fun HTMLBuilder.pre(block: HTMLBuilder.() -> Unit = {}) = tag("pre", bloc
  * @param classes the `class` attribute
  * @param block the builder action
  */
-inline fun HTMLBuilder.table(classes: String? = null, block: HTMLBuilder.() -> Unit = {}) =
+inline fun HTMLBuilder.table(classes: String? = null, block: HTMLBuilderBlock = {}) =
     tag("table", mapOf("class" to classes), block)
 
 /**
@@ -148,28 +200,28 @@ inline fun HTMLBuilder.table(classes: String? = null, block: HTMLBuilder.() -> U
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.thead(block: HTMLBuilder.() -> Unit = {}) = tag("thead", block = block)
+inline fun HTMLBuilder.thead(block: HTMLBuilderBlock = {}) = tag("thead", block = block)
 
 /**
  * Appends a `<tbody>` tag.
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.tbody(block: HTMLBuilder.() -> Unit = {}) = tag("tbody", block = block)
+inline fun HTMLBuilder.tbody(block: HTMLBuilderBlock = {}) = tag("tbody", block = block)
 
 /**
  * Appends a `<tr>` tag.
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.tr(block: HTMLBuilder.() -> Unit = {}) = tag("tr", block = block)
+inline fun HTMLBuilder.tr(block: HTMLBuilderBlock = {}) = tag("tr", block = block)
 
 /**
  * Appends a `<th>` tag.
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.th(block: HTMLBuilder.() -> Unit = {}) = tag("th", block = block)
+inline fun HTMLBuilder.th(block: HTMLBuilderBlock = {}) = tag("th", block = block)
 
 /**
  * Appends a `<td>` tag.
@@ -178,7 +230,7 @@ inline fun HTMLBuilder.th(block: HTMLBuilder.() -> Unit = {}) = tag("th", block 
  * @param style the `style` attribute
  * @param block the builder action
  */
-inline fun HTMLBuilder.td(classes: String? = null, style: String? = null, block: HTMLBuilder.() -> Unit = {}) =
+inline fun HTMLBuilder.td(classes: String? = null, style: String? = null, block: HTMLBuilderBlock = {}) =
     tag("td", mapOf("class" to classes, "style" to style), block)
 
 /**
@@ -190,7 +242,7 @@ inline fun HTMLBuilder.td(classes: String? = null, style: String? = null, block:
  * @param style the `style` attribute
  * @param block the builder action
  */
-inline fun HTMLBuilder.div(id: String? = null, onClick: String? = null, classes: String? = null, style: String? = null, block: HTMLBuilder.() -> Unit = {}) =
+inline fun HTMLBuilder.div(id: String? = null, onClick: String? = null, classes: String? = null, style: String? = null, block: HTMLBuilderBlock = {}) =
     tag("div", mapOf("id" to id, "onclick" to onClick, "class" to classes, "style" to style), block)
 
 /**
@@ -200,7 +252,7 @@ inline fun HTMLBuilder.div(id: String? = null, onClick: String? = null, classes:
  * @param style the `style` attribute
  * @param block the builder action
  */
-inline fun HTMLBuilder.span(classes: String? = null, style: String? = null, block: HTMLBuilder.() -> Unit = {}) =
+inline fun HTMLBuilder.span(classes: String? = null, style: String? = null, block: HTMLBuilderBlock = {}) =
     tag("span", mapOf("class" to classes, "style" to style), block)
 
 /**
@@ -212,7 +264,7 @@ inline fun HTMLBuilder.span(classes: String? = null, style: String? = null, bloc
  * @param ariaLabel the `aria-label` attribute
  * @param block the builder action
  */
-inline fun HTMLBuilder.a(id: String? = null, href: String? = null, classes: String? = null, ariaLabel: String? = null, block: HTMLBuilder.() -> Unit = {}) =
+inline fun HTMLBuilder.a(id: String? = null, href: String? = null, classes: String? = null, ariaLabel: String? = null, block: HTMLBuilderBlock = {}) =
     tag("a", mapOf("id" to id, "href" to href, "class" to classes, "aria-label" to ariaLabel), block)
 
 /**
@@ -224,7 +276,7 @@ inline fun HTMLBuilder.a(id: String? = null, href: String? = null, classes: Stri
  * @param spellCheck the `spellcheck` attribute
  * @param placeholder the `placeholder` attribute
  */
-fun HTMLBuilder.input(id: String? = null, type: String? = null, classes: String? = null, spellCheck: Boolean? = null, placeholder: String? = null) =
+fun HTMLBuilder.input(id: String? = null, type: String? = null, classes: String? = null, spellCheck: Boolean = false, placeholder: String? = null) =
     tag("input", mapOf("id" to id, "type" to type, "class" to classes, "spellcheck" to spellCheck, "placeholder" to placeholder))
 
 /**
@@ -232,14 +284,14 @@ fun HTMLBuilder.input(id: String? = null, type: String? = null, classes: String?
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.nav(block: HTMLBuilder.() -> Unit = {}) = tag("nav", block = block)
+inline fun HTMLBuilder.nav(block: HTMLBuilderBlock = {}) = tag("nav", block = block)
 
 /**
  * Appends a `<footer>` tag.
  *
  * @param block the builder action
  */
-inline fun HTMLBuilder.footer(block: HTMLBuilder.() -> Unit = {}) = tag("footer", block = block)
+inline fun HTMLBuilder.footer(block: HTMLBuilderBlock = {}) = tag("footer", block = block)
 
 /**
  * Appends a `<meta>` tag.
@@ -256,7 +308,7 @@ fun HTMLBuilder.meta(name: String? = null, content: String? = null) =
  * @param src the `src` attribute
  * @param block the builder action
  */
-inline fun HTMLBuilder.script(src: String? = null, block: HTMLBuilder.() -> Unit = {}) =
+inline fun HTMLBuilder.script(src: String? = null, block: HTMLBuilderBlock = {}) =
     tag("script", mapOf("src" to src), block)
 
 /**
